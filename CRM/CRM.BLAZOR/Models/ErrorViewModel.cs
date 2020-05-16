4bd98e25dd084567d1213fc568ee6b66573d210d
Amangeldi <Amangeldi.96.ru@inbox.ru>
2020-05-16 15:24:44 +0500

    architecture DAL

---
+++ CRM/CRM.BLAZOR/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRM.BLAZOR.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ CRM/CRM.DAL/Entities/Log.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CRM.DAL.Entities
+{
+    class Log
+    {
+    }
+}
